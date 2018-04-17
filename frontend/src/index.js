ccfc5a44e8092d8fc5983ef6b5b707bdb1fd72e4
github username <github email address>
2018-04-17 14:23:08 -0400

    frontend flow stuff

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,9 +4,21 @@ const twoD = canvas.getContext('2d')
 
 window.addEventListener('load', function() {
 
-  const start = new Start();
-  start.render()
+  // const start = new Start();
+  // start.render()
 
   // const game = new Game();
+  function displayCharacterBio(e){
+    
+  }
 
+  function startGame(e){
+    const chooseCharacter = document.querySelector('#choose-character-screen')
+    document.querySelector('#start-screen').style.display = 'none'
+    chooseCharacter.style.display = 'inline'
+    chooseCharacter.addEventListener('hover', displayCharacterBio)
+  }
+
+
+  document.querySelector("#start-button-div").addEventListener('click', startGame)
 })
