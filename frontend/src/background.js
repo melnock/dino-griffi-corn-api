321f5bb5bae911a9d5932f12c481c288d2ac079e
alec-petros <a.petros@verizon.net>
2018-04-19 16:29:31 -0400

    lol

--- frontend/src/background.js
+++ frontend/src/background.js
@@ -3,10 +3,17 @@ class Background {
     this.counter = 0
     this.sunY = 350
     this.cityY = 200
+    this.skellY = 200
   }
 
   render(game) {
     twoD.drawImage(document.querySelector('#sky'), 0, 0, 640, 450)
+    if (this.sunY > 80) {
+      twoD.drawImage(document.querySelector('#skelinton'), 350, this.sunY -= 0.1, 200, 200)
+    } else {
+      twoD.drawImage(document.querySelector('#skelinton'), 350, this.sunY, 200, 200)
+      this.level = 3
+    }
     if (this.sunY > 80) {
       twoD.drawImage(document.querySelector('#sun'), 220, this.sunY -= 0.1, 200, 200)
     } else {
@@ -19,7 +26,7 @@ class Background {
       twoD.drawImage(document.querySelector('#city'), 0, this.cityY, 640, 150)
       this.level = Math.max(2, this.level)
     }
-    
+
     twoD.lineWidth = "2"
     this.drawLine(0, 240, 640, 240)
     this.vertLine(320)
