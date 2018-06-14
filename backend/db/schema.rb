3bb8907161c3dcccd2fd976e8547ebccafe4523f
github username <github email address>
2018-06-14 16:50:11 -0400

    switched DB to Postgres

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2018_04_19_131403) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "characters", force: :cascade do |t|
     t.string "name"
     t.text "sprites"
